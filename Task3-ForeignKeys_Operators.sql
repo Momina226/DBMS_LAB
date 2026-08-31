033ebf64361425727ff626e88b749f5394caca8b
Momina <n230630@rguktn.ac.in>
2026-08-31 15:51:39 +0530

    Add files via upload
     Completed DBMS Lab Task-3

---
+++ Task3-ForeignKeys_Operators.sql
@@ -0,0 +1,52 @@
+USE playstoredb;
+ALTER TABLE Apps 
+ADD CONSTRAINT fk_developer 
+FOREIGN KEY (DeveloperID) 
+REFERENCES Developers(DeveloperID); 
+ALTER TABLE Apps
+ADD CONSTRAINT fk_publisher
+FOREIGN KEY (PublisherID)
+REFERENCES Publishers(PublisherID);
+ALTER TABLE Apps
+ADD CONSTRAINT fk_category
+FOREIGN KEY (CategoryID)
+REFERENCES Categories(CategoryID);
+SELECT * FROM Apps;
+SELECT * FROM Apps 
+WHERE Rating >4.5; 
+SELECT * FROM Apps
+WHERE Price = 0;
+SELECT * FROM Apps
+WHERE CategoryID=305;
+SELECT * FROM Apps
+WHERE Downloads > 500000000;
+SELECT * FROM Apps
+WHERE Rating between 4.3 AND 4.7 ;
+SELECT * FROM Apps
+WHERE Price IN(0,299);
+SELECT * FROM Apps
+WHERE AppName LIKE 'G%';
+SELECT * FROM Apps
+WHERE AppName LIKE 'Google%';
+SELECT * FROM Apps
+WHERE Rating > 4.0 AND Downloads > 500000000;
+SELECT * FROM Apps
+WHERE CategoryID=301 OR CategoryID=305;
+SELECT * FROM Apps
+WHERE NOT AppName LIKE 'G%'; 
+SELECT * FROM Apps
+WHERE Rating < 4.5 OR Downloads >  1000000000 ;
+SELECT * FROM Apps
+WHERE AppName LIKE '%a%';
+SELECT * FROM Apps
+WHERE Price BETWEEN 0 AND 300;
+SELECT * FROM Apps
+WHERE PublisherID IN(201,204);
+INSERT INTO Apps
+VALUES
+(1050,"testing",109,209,309,4.9,10000,150);
+SELECT * FROM Apps;
+SELECT * FROM Apps
+WHERE NOT CategoryID=305;
+
+
